6b074dd4ba7986b43ebb68dccee8940180825695
lexermal <contact@weixler.me>
2025-05-01 22:11:48 +0200

    feat: exporing eventbus

--- src/core.ts
+++ src/core.ts
@@ -7,3 +7,4 @@ export * from "./plugin/PluginController";
 export * from "./utils/difficultyConverter";
 export * from "./utils/PluginUtils";
 export * from "./worker/WorkerSetup";
+export * from "./plugin/fromRimori/EventBus";
