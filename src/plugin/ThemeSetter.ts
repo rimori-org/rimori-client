c07353a6f14529be855af2f2c703609958786e4e
lexermal <contact@weixler.me>
2025-05-03 16:39:06 +0200

    feat: removing sidebar difference

--- src/plugin/ThemeSetter.ts
+++ src/plugin/ThemeSetter.ts
@@ -2,8 +2,6 @@ export function setTheme() {
   const urlParams = new URLSearchParams(window.location.search);
 
   let theme = urlParams.get('theme');
-  const isSidebar = urlParams.get('applicationMode') === "sidebar";
-
   if (!theme || theme === 'system') {
     theme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
   }
@@ -11,7 +9,8 @@ export function setTheme() {
   document.documentElement.classList.add("dark:text-gray-200", "bg-white");
 
   if (theme === 'dark') {
-    document.documentElement.classList.add('dark', isSidebar ? "bg-gray-920" : "bg-gray-950");
-    document.documentElement.style.background = isSidebar ? "rgb(6, 12, 30)" : "rgb(3, 7, 18)";
+    document.documentElement.setAttribute("data-theme", "dark");
+    document.documentElement.classList.add('dark', "bg-gray-950");
+    document.documentElement.style.background = "hsl(var(--background))";
   }
 }
