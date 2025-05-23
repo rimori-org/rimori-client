9a868e3033a00c4691f08e833b616b294a74deb9
Alexander Weixler <alexander.weixler@minut.com>
2025-05-23 22:06:57 +0200

    fix: plugins emitting

--- src/plugin/PluginController.ts
+++ src/plugin/PluginController.ts
@@ -7,127 +7,129 @@ import { setTheme } from './ThemeSetter';
 declare const WorkerGlobalScope: any;
 
 interface SupabaseInfo {
-    url: string,
-    key: string,
-    token: string,
-    expiration: Date,
-    tablePrefix: string,
-    pluginId: string
+  url: string,
+  key: string,
+  token: string,
+  expiration: Date,
+  tablePrefix: string,
+  pluginId: string
 }
 
 export class PluginController {
-    private static client: RimoriClient;
-    private static instance: PluginController;
-    private communicationSecret: string | null = null;
-    private supabase: SupabaseClient | null = null;
-    private supabaseInfo: SupabaseInfo | null = null;
-    private pluginId: string;
+  private static client: RimoriClient;
+  private static instance: PluginController;
+  private communicationSecret: string | null = null;
+  private supabase: SupabaseClient | null = null;
+  private supabaseInfo: SupabaseInfo | null = null;
+  private pluginId: string;
 
-    private constructor(pluginId: string) {
-        this.pluginId = pluginId;
-        this.getClient = this.getClient.bind(this);
+  private constructor(pluginId: string) {
+    this.pluginId = pluginId;
+    this.getClient = this.getClient.bind(this);
 
-        if (typeof WorkerGlobalScope === 'undefined') {
-            setTheme();
-        }
-
-        window.addEventListener("message", (event) => {
-            // console.log("client: message received", event);
-            const { topic, sender, data, eventId } = event.data.event as EventBusMessage;
-
-            // skip forwarding messages from own plugin
-            if (sender === pluginId) return;
-
-            EventBus.emit(sender, topic, data, eventId);
-        });
-
-        EventBus.on("*", (event) => {
-            // skip messages which are not from the own plugin
-            if (event.sender !== this.pluginId) return;
-            if (event.topic.startsWith("self.")) return;
-            window.parent.postMessage({ event, secret: this.getSecret() }, "*")
-        });
+    if (typeof WorkerGlobalScope === 'undefined') {
+      setTheme();
     }
 
-    public static async getInstance(sender: string): Promise<RimoriClient> {
-        if (!PluginController.instance) {
-            PluginController.instance = new PluginController(sender);
-            PluginController.client = await RimoriClient.getInstance(PluginController.instance);
-        }
-        return PluginController.client;
+    window.addEventListener("message", (event) => {
+      // console.log("client: message received", event);
+      const { topic, sender, data, eventId } = event.data.event as EventBusMessage;
+
+      // skip forwarding messages from own plugin
+      if (sender === pluginId) return;
+
+      EventBus.emit(sender, topic, data, eventId);
+    });
+
+    EventBus.on("*", (event) => {
+      // skip messages which are not from the own plugin
+      if (event.sender !== this.pluginId) return;
+      if (event.topic.startsWith("self.")) return;
+      window.parent.postMessage({ event, secret: this.getSecret() }, "*")
+    });
+  }
+
+  public static async getInstance(sender: string): Promise<RimoriClient> {
+    if (!PluginController.instance) {
+      PluginController.instance = new PluginController(sender);
+      PluginController.client = await RimoriClient.getInstance(PluginController.instance);
+    }
+    return PluginController.client;
+  }
+
+  private getSecret() {
+    if (!this.communicationSecret) {
+      const secret = new URLSearchParams(window.location.search).get("secret");
+      if (!secret) {
+        throw new Error("Communication secret not found in URL as query parameter");
+      }
+      this.communicationSecret = secret;
+    }
+    return this.communicationSecret;
+  }
+
+  public async getClient(): Promise<{ supabase: SupabaseClient, tablePrefix: string, pluginId: string }> {
+    if (
+      this.supabase &&
+      this.supabaseInfo &&
+      this.supabaseInfo.expiration > new Date()
+    ) {
+      return { supabase: this.supabase, tablePrefix: this.supabaseInfo.tablePrefix, pluginId: this.supabaseInfo.pluginId };
     }
 
-    private getSecret() {
-        if (!this.communicationSecret) {
-            const secret = new URLSearchParams(window.location.search).get("secret");
-            if (!secret) {
-                throw new Error("Communication secret not found in URL as query parameter");
-            }
-            this.communicationSecret = secret;
-        }
-        return this.communicationSecret;
+    const { data } = await EventBus.request<SupabaseInfo>(this.pluginId, "global.supabase.requestAccess");
+    this.supabaseInfo = data;
+    this.supabase = createClient(this.supabaseInfo.url, this.supabaseInfo.key, {
+      accessToken: () => Promise.resolve(this.getToken())
+    });
+
+    return { supabase: this.supabase, tablePrefix: this.supabaseInfo.tablePrefix, pluginId: this.supabaseInfo.pluginId };
+  }
+
+  public async getToken() {
+    if (this.supabaseInfo && this.supabaseInfo.expiration && this.supabaseInfo.expiration > new Date()) {
+      return this.supabaseInfo.token;
     }
 
-    public async getClient(): Promise<{ supabase: SupabaseClient, tablePrefix: string, pluginId: string }> {
-        if (
-            this.supabase &&
-            this.supabaseInfo &&
-            this.supabaseInfo.expiration > new Date()
-        ) {
-            return { supabase: this.supabase, tablePrefix: this.supabaseInfo.tablePrefix, pluginId: this.supabaseInfo.pluginId };
-        }
+    const { data } = await EventBus.request<{ token: string, expiration: Date }>(this.pluginId, "global.supabase.requestAccess");
 
-        const { data } = await EventBus.request<SupabaseInfo>(this.pluginId, "global.supabase.requestAccess");
-        this.supabaseInfo = data;
-        this.supabase = createClient(this.supabaseInfo.url, this.supabaseInfo.key, {
-            accessToken: () => Promise.resolve(this.getToken())
-        });
-
-        return { supabase: this.supabase, tablePrefix: this.supabaseInfo.tablePrefix, pluginId: this.supabaseInfo.pluginId };
+    if (!this.supabaseInfo) {
+      throw new Error("Supabase info not found");
     }
 
-    public async getToken() {
-        if (this.supabaseInfo && this.supabaseInfo.expiration && this.supabaseInfo.expiration > new Date()) {
-            return this.supabaseInfo.token;
-        }
+    this.supabaseInfo.token = data.token;
+    this.supabaseInfo.expiration = data.expiration;
 
-        const { data } = await EventBus.request<{ token: string, expiration: Date }>(this.pluginId, "global.supabase.requestAccess");
+    return this.supabaseInfo.token;
+  }
 
-        if (!this.supabaseInfo) {
-            throw new Error("Supabase info not found");
-        }
-
-        this.supabaseInfo.token = data.token;
-        this.supabaseInfo.expiration = data.expiration;
-
-        return this.supabaseInfo.token;
+  public getSupabaseUrl() {
+    if (!this.supabaseInfo) {
+      throw new Error("Supabase info not found");
     }
 
-    public getSupabaseUrl() {
-        if (!this.supabaseInfo) {
-            throw new Error("Supabase info not found");
-        }
+    return this.supabaseInfo.url;
+  }
 
-        return this.supabaseInfo.url;
+  public getGlobalEventTopic(preliminaryTopic: string) {
+    if (preliminaryTopic.startsWith("global.")) {
+      return preliminaryTopic;
+    }
+    if (preliminaryTopic.startsWith("self.")) {
+      return preliminaryTopic;
+    }
+    const topicParts = preliminaryTopic.split(".");
+    if (topicParts.length === 3) {
+      if (!topicParts[0].startsWith("pl") && topicParts[0] !== "global") {
+        throw new Error("The event topic must start with the plugin id or 'global'.");
+      }
+      return preliminaryTopic;
+    } else if (topicParts.length > 3) {
+      throw new Error(`The event topic must consist of 3 parts. <pluginId>.<topic area>.<action>. Received: ${preliminaryTopic}`);
     }
 
-    public getGlobalEventTopic(preliminaryTopic: string) {
-        if (preliminaryTopic.startsWith("global.")) {
-            return preliminaryTopic;
-        }
-        if (preliminaryTopic.startsWith("self.")) {
-            return preliminaryTopic;
-        }
-        const topicParts = preliminaryTopic.split(".");
-        if (topicParts.length === 3) {
-            if (![this.supabaseInfo?.pluginId, "global"].includes(topicParts[0])) {
-                throw new Error("The event topic must start with the plugin id or 'global'.");
-            }
-            return preliminaryTopic;
-        }
-
-        const topicRoot = this.supabaseInfo?.pluginId ?? "global";
-        return `${topicRoot}.${preliminaryTopic}`;
-    }
+    const topicRoot = this.supabaseInfo?.pluginId ?? "global";
+    return `${topicRoot}.${preliminaryTopic}`;
+  }
 
 }
