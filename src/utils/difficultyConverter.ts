97fb0008b87d3e3aa9a07116b9d7a13d36b7fff7
lexermal <contact@weixler.me>
2025-06-20 21:27:24 +0200

    fix: difficulty level caluclator fixed

--- src/utils/difficultyConverter.ts
+++ src/utils/difficultyConverter.ts
@@ -3,13 +3,13 @@ const codes = ["Pre-A1", "A1", "A2", "B1", "B2", "C1", "C2", "Post-C2"];
 export type LanguageLevel = "Pre-A1" | "A1" | "A2" | "B1" | "B2" | "C1" | "C2" | "Post-C2";
 
 export function getDifficultyLevel(difficulty: LanguageLevel): number {
-    return codes.indexOf(difficulty) + 1;
+  return codes.indexOf(difficulty) + 1;
 }
 
 export function getDifficultyLabel(difficulty: number): LanguageLevel {
-    return codes[difficulty] as LanguageLevel;
+  return codes[difficulty] as LanguageLevel;
 }
 
 export function getNeighborDifficultyLevel(difficulty: LanguageLevel, difficultyAdjustment: number): LanguageLevel {
-    return getDifficultyLabel(getDifficultyLevel(difficulty) + difficultyAdjustment);
+  return getDifficultyLabel(getDifficultyLevel(difficulty) + difficultyAdjustment - 1);
 }
