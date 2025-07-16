9b312360a30667be3f5708eca94b2e3bfe241905
lexermal <contact@weixler.me>
2025-07-16 21:35:40 +0200

    feat: dark mode detection

--- src/plugin/ThemeSetter.ts
+++ src/plugin/ThemeSetter.ts
@@ -1,16 +1,20 @@
 export function setTheme() {
-  const urlParams = new URLSearchParams(window.location.search);
-
-  let theme = urlParams.get('theme');
-  if (!theme || theme === 'system') {
-    theme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-  }
-
   document.documentElement.classList.add("dark:text-gray-200");
 
-  if (theme === 'dark') {
+  if (isDarkTheme()) {
     document.documentElement.setAttribute("data-theme", "dark");
     document.documentElement.classList.add('dark', "dark:bg-gray-950");
     document.documentElement.style.background = "hsl(var(--background))";
   }
+}
+
+export function isDarkTheme(): boolean {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  let theme = urlParams.get('theme');
+  if (!theme || theme === 'system') {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+
+  return theme === 'dark';
 }
